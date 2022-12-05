335453f65c47b441c1d6505c4cd79cdd7cebdfa0
Mohammed Sanad <96536944+Muhammed-7485@users.noreply.github.com>
2022-12-05 15:44:01 +0200

    Added filter_buttons,stylling.deleteDone_button

--- src/compomemts/tasks.jsx
+++ src/compomemts/tasks.jsx
@@ -7,10 +7,15 @@ class Tasks extends Component {
       { title: "task 1", id: 1, done: false },
       { title: "task 2", id: 2, done: false },
       { title: "task 3", id: 3, done: false },
+      { title: "task 4", id: 4, done: true },
+      { title: "task 5", id: 5, done: true },
     ],
     value: "",
     title: "",
-    counter: 3,
+    counter: 5,
+    filterAll: true,
+    filterDone: false,
+    filterTodo: false,
   };
   render() {
     return (
@@ -34,15 +39,45 @@ class Tasks extends Component {
         </form>
         <hr />
         <h1 className="text-center">TodoList</h1>
-        {this.state.allTasks.map((task) => (
-          <Task
-            key={task.id}
-            task={task}
-            onDelete={this.handleDelete}
-            onDone={this.handleDone}
-            onEdit={this.handleEdit}
-          />
-        ))}
+        <div className="m-3 d-flex justify-content-between">
+          <button
+            className={this.getAllButtonClasses()}
+            style={{ padding: "3px 70px" }}
+            onClick={this.handelSeeAll}
+          >
+            All
+          </button>
+          <button
+            className={this.getDoneButtonClasses()}
+            style={{ padding: "3px 70px" }}
+            onClick={this.handelSeeDone}
+          >
+            Done
+          </button>
+          <button
+            className={this.getTodoButtonClasses()}
+            style={{ padding: "3px 70px" }}
+            onClick={this.handelSeeTodo}
+          >
+            Todo
+          </button>
+        </div>
+        {this.state.allTasks
+          .filter(
+            (task) =>
+              (this.state.filterDone && task.done === true) ||
+              this.state.filterAll ||
+              (this.state.filterTodo && task.done === false)
+          )
+          .map((task) => (
+            <Task
+              key={task.id}
+              task={task}
+              onDelete={this.handleDelete}
+              onDone={this.handleDone}
+              onEdit={this.handleEdit}
+            />
+          ))}
 
         <div className="m-3 d-flex justify-content-between">
           <button
@@ -64,6 +99,24 @@ class Tasks extends Component {
     );
   }
 
+  // ******************************   Classes   **********************************
+  getDoneButtonClasses() {
+    let classes = "btn btn-sm btn-";
+    classes += this.state.filterDone ? "primary" : "secondary";
+    return classes;
+  }
+  getTodoButtonClasses() {
+    let classes = "btn btn-sm btn-";
+    classes += this.state.filterTodo ? "primary" : "secondary";
+    return classes;
+  }
+  getAllButtonClasses() {
+    let classes = "btn btn-sm btn-";
+    classes += this.state.filterAll ? "primary" : "secondary";
+    return classes;
+  }
+
+  // ******************************   Submit New Task   **********************************
   handleSubmit = (e) => {
     console.log("submit called");
     e.preventDefault();
@@ -90,6 +143,7 @@ class Tasks extends Component {
     }));
   };
 
+  // ******************************  General Buttons   **********************************
   handleDeleteAll = () => {
     console.log("onDeleteAll called");
     const allTasks = [];
@@ -98,16 +152,12 @@ class Tasks extends Component {
 
   handelDeleteDone = () => {
     console.log("onDeleteDone called");
-    // const allTasks = this.state.allTasks.filter((c) => c.done === false);
-    this.state.allTasks.forEach((task) => {
-      console.log(this.state.allTasks);
-      if (task.done) {
-        this.handleDelete(task.id);
-      }
-    });
-    // this.setState({ allTasks });
+    console.log(this.state.allTasks);
+    const allTasks = this.state.allTasks.filter((c) => c.done === false);
+    this.setState({ allTasks });
   };
 
+  // ******************************   Inline Buttons   **********************************
   handleDelete = (taskId) => {
     console.log("onDelete called");
     console.log(this.state.allTasks);
@@ -126,17 +176,31 @@ class Tasks extends Component {
       "onDone called",
       "Not Working i can't get element index in array (-1)"
     );
-    // console.log("allTasks", this.state.allTasks);
-    const tasks = [...this.state.allTasks];
+    // // console.log("allTasks", this.state.allTasks);
+    // const tasks = [...this.state.allTasks];
     // console.log("Tasks", tasks);
-    const task = { title, id, done };
+    // const task = { title, id, done };
     // console.log("task", task);
-    const index = this.state.allTasks.indexOf(task);
+    // const index = this.state.allTasks.indexOf(task);
     // console.log(index);
-    // tasks[index].done = !task.done;
-    // tasks[index] = { ...task };
-    // console.log("allTasks", this.state.allTasks);
-    // console.log("Tasks", tasks);
+    // // tasks[index].done = !task.done;
+    // // tasks[index] = { ...task };
+    // // console.log("allTasks", this.state.allTasks);
+    // // console.log("Tasks", tasks);
+  };
+
+  // ******************************   Filters   **********************************
+  handelSeeDone = () => {
+    console.log("onSeeDone called");
+    this.setState({ filterDone: true, filterAll: false, filterTodo: false });
+  };
+  handelSeeTodo = () => {
+    console.log("onSeeTodo called");
+    this.setState({ filterDone: false, filterAll: false, filterTodo: true });
+  };
+  handelSeeAll = () => {
+    console.log("onSeeAll called");
+    this.setState({ filterDone: false, filterAll: true, filterTodo: false });
   };
 }
 
